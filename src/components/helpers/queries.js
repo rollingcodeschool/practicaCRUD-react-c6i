40aed5b7affc5bf72058bdc1b5802539f05f7293
Emilse Arias <emilsearias@gmail.com>
2022-10-11 17:01:16 -0300

    agregamos de comentario los tipos de peticiones

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -2,7 +2,13 @@
 
 const URL ='http://localhost:3004/productos';
 
-// peticion GET que trae todos los productos
+// tipos de peticiones
+// peticion GET que trae todos los productos o un producto
+// peticion POST, crear un producto, login
+// peticion DELETE, peticion para borrar
+// peticion PUT, peticion que pide modificar un producto
+
+
 export const consultarAPI = async()=>{
     try{
         const respuesta = await fetch(URL);
