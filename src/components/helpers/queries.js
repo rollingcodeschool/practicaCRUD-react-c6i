75dfc350e41c7135aed4d81d0ec279f1161d6763
Emilse Arias <emilsearias@gmail.com>
2022-11-03 18:59:44 -0300

    agregamos la variable de entorno con el endopoint del proyecto de produccion

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,7 +1,7 @@
 //archivo que nos siver para hacer las consultas a la api (json-server)
 
 // const URL ='http://localhost:3004/productos'; //json-server
-const URL ='http://localhost:4000/apicafe/productos';
+const URL = process.env.REACT_APP_API_CAFE;
 const URL_USER ='http://localhost:3004/usuarios';
 
 // tipos de peticiones
